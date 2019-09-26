7c4d2846fadae9d0ab59700483d7d6396b4fc9c0
sedies <sedi.september@gmail.com>
2019-09-26 09:55:56 +0900

    useEffect 예제주석추가

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -10,7 +10,7 @@ const Info = () => {
       console.log('cleanup');
       console.log(name);
     };
-  }, []);
+  }, []); //두번째 파라미터로 빈배열을 넣으면 마운트완료후 최초 한번만 실행되고, 업데이트 추적이 되지 않는다. 만약 배열안에 props나 state에서 관리하는 상태값을 넣으면 해당하는 내용을 추척하여 업데이트한다. return함수는 언마운트때만 실행 하고 싶다면 두번째 파라미터에 빈배열을 넣어주면 된다.
 
   const onChangeName = e => {
     setName(e.target.value);
