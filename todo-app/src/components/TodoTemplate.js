3df26dc7b8d25e4b8ce86b434fb8bbe444d36472
sedies <sedi.september@gmail.com>
2019-09-28 23:36:39 +0900

    TodoTemplate 배경 및 기본스타일 적용

---
+++ todo-app/src/components/TodoTemplate.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TodoWrap = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 6rem;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+const AppTitle = styled.div`
+  background: #22b8cf;
+  color: white;
+  height: 4rem;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Content = styled.div`
+  background: white;
+`;
+
+const TodoTemplate = ({ children }) => {
+  return (
+    <TodoWrap>
+      <AppTitle>일정 관리</AppTitle>
+      <Content>{children}</Content>
+    </TodoWrap>
+  );
+};
+
+export default TodoTemplate;
